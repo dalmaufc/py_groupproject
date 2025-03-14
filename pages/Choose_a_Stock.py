b355889651d01d759d4d0f5b573e488737e77b05
dalmaufc <dalmaufc@student.ie.edu>
2025-03-14 15:20:04 +0100

    Update Choose_a_Stock.py

--- pages/Choose_a_Stock.py
+++ pages/Choose_a_Stock.py
@@ -121,6 +121,8 @@ merged_df.dropna(subset=["close", "p_e_ratio", "sma_50"], inplace=True)
 if "fiscal_period" in merged_df.columns:
     merged_df = merged_df.drop(columns=["fiscal_period"])
 
+st.dataframe(merged_df.set_index("date"))
+
 # Display only the last 10 rows
 st.subheader(f"📊 API Live Data for {selected_stock} (Latest 10 Daily Closing)")
 st.dataframe(merged_df.tail(10))
